eaa7c6edba66b715fa7eaf8f1829d782631b05c5
aksoydiyar <aksoydiyar000@gmail.com>
2024-03-09 13:03:39 +0300

    kurs Description değiştirildi

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -9,7 +9,7 @@ namespace basics.Models
         {
             _courses=new List<Course>()
             {
-            new Course() {Id = 0,Title= "asp kursu",Description="güzel bir kurs",Image="1.jpeg"},
+            new Course() {Id = 0,Title= "asp kursu",Description="güzel bir kurs gerçekten",Image="1.jpeg"},
             new Course() {Id = 1,Title= "php kursu",Description="güzel bir kurs",Image="1.jpeg"},
             new Course() {Id = 2,Title= "java kursu",Description="güzel bir kurs",Image="1.jpeg"},
 
